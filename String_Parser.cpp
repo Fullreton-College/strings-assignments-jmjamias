9d39428bca92641fb335b307072ba2f86a880b6c
Jamie Jamias <jamiemanabatjamias@gmail.com>
2026-03-15 15:14:12 -0700

    Assignment Complete

--- String_Parser.cpp
+++ String_Parser.cpp
@@ -4,10 +4,43 @@
 #include <sstream> // For parsing lines
 using namespace std;
 
-void parseCSV(const string&);
+void parseCSV(const string&);          //function declaration
 
 int main() {
-   parseCSV("students.csv");
+   parseCSV("students.csv");        //call with filename
 
    return 0;
+}
+
+void parseCSV(const string& filename)
+{
+   ifstream inFile(filename);    //open the CSV file
+
+   if (!inFile.is_open())           //check if file opened
+   {
+      cout << "Error: Could not open "
+            << filename << endl;
+            return;           //exit if filie not found
+   }
+
+   string line;
+   int lineNum = 0;
+
+   while (getline(inFile, line))
+{
+   lineNum++;
+   stringstream ss(line);
+   string field;
+
+   cout << "Row " << lineNum << ": ";
+
+   while (getline(ss, field, ','))
+   {
+      cout << field << " ";
+   }
+   cout << endl;
+}
+
+   inFile.close();
+   cout << "\nTotal rows read: " << lineNum << endl;
 }
